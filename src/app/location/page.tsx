0349b22f16119f24f0f272a6187389de941fa271
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2025-02-11 20:54:22 +0800

    update lotacion

---
+++ src/app/location/page.tsx
@@ -0,0 +1,12 @@
+import MapLocation from "@/app/component/Maplocation";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Your Location on Map</h1>
+      <MapLocation />
+    </div>
+  );
+};
+
+export default Page;
