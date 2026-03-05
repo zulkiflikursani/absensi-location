9b41da4ccc8dbc9d40eae2cb920a8b81515f72c2
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2026-03-05 13:12:47 +0800

    fix update

--- src/app/api/getdataAdmin/route.ts
+++ src/app/api/getdataAdmin/route.ts
@@ -12,44 +12,64 @@ export async function POST(req: NextRequest) {
   const prisma = new PrismaClient();
 
   try {
-    const query = `WITH RECURSIVE DateSeries AS (
-        SELECT CAST('${data.bulan}' AS DATE) AS tanggal  -- Start date (1st of the month)
-        UNION ALL
-        SELECT tanggal + INTERVAL 1 DAY
-        FROM DateSeries
-        WHERE tanggal < LAST_DAY('${data.bulan}') -- End date (last day of the month)
-    )
+    const query = `WITH RECURSIVE
+    DateSeries AS(
     SELECT
-        u.id AS idUser,
-        u.username,
-        u.full_name,
-        u.bagian,
-        ds.tanggal,
-        CONCAT(ds.tanggal, ' 07:30:00')  AS jam_masuk,
-        CONCAT(ds.tanggal, ' 16:00:00') AS jam_pulang,
-        min(m.waktu) AS waktu_masuk,  -- Earliest 'masuk' time
-        max(k.waktu) AS waktu_keluar,   -- Latest 'keluar' time
-       CAST(TIMESTAMPDIFF(MINUTE, MIN(m.waktu), MAX(k.waktu)) AS UNSIGNED) AS selisih_menit,
-         CAST(TIMESTAMPDIFF(
-        MINUTE,
-        CONCAT(ds.tanggal, ' 07:30:00'), -- 08:00 Asia/Makassar
-        MIN(m.waktu)  -- Waktu masuk (dalam UTC+7)
-    ) AS SIGNED) AS terlambat
-    FROM
-      users u
-    CROSS JOIN
-        DateSeries ds
-    LEFT JOIN
-        masuk m ON DATE(ds.tanggal) =DATE(m.waktu) AND m.idUser = u.id 
-    LEFT JOIN
-        keluar k ON DATE(ds.tanggal) = DATE(k.waktu)AND k.idUser = u.id
-    where
-        u.id != '2' 
-        and u.bagian = '${data.bagian}'
-    GROUP BY
-        u.id, ds.tanggal
-    ORDER BY
-        u.id, ds.tanggal`;
+        CAST('${data.bulan}' AS DATE) AS tanggal -- Start date (1st of the month)
+    UNION ALL
+SELECT
+    tanggal + INTERVAL 1 DAY
+FROM
+    DateSeries
+WHERE
+    tanggal < LAST_DAY('${data.bulan}') -- End date (last day of the month)
+)
+SELECT
+    u.id AS idUser,
+    u.username,
+    u.full_name,
+    u.bagian,
+    ds.tanggal,
+    CONCAT(ds.tanggal, ' 07:30:00') AS jam_masuk,
+    CONCAT(ds.tanggal, ' 16:00:00') AS jam_pulang,
+    MIN(m.waktu) AS waktu_masuk,
+    -- Earliest 'masuk' time
+    MAX(k.waktu) AS waktu_keluar,
+    -- Latest 'keluar' time
+    CAST(
+        COALESCE(
+            TIMESTAMPDIFF(
+                MINUTE,
+                MIN(m.waktu),
+                MAX(k.waktu)
+            ),
+            0
+        ) AS UNSIGNED
+    ) AS selisih_menit,
+    CAST(
+        COALESCE(
+            TIMESTAMPDIFF(
+                MINUTE,
+                CONCAT(ds.tanggal, ' 07:30:00'),
+                MIN(m.waktu)
+            ),
+            0
+        ) AS SIGNED
+    ) AS terlambat
+FROM
+    users u
+CROSS JOIN DateSeries ds LEFT JOIN masuk m ON
+    DATE(ds.tanggal) = DATE(m.waktu) AND m.idUser = u.id
+LEFT JOIN keluar k ON
+    DATE(ds.tanggal) = DATE(k.waktu) AND k.idUser = u.id
+WHERE
+    u.id != '2' AND u.bagian = '${data.bagian}'
+GROUP BY
+    u.id,
+    ds.tanggal
+ORDER BY
+    u.id,
+    ds.tanggal`;
     console.log(query);
     const result = await prisma.$queryRawUnsafe(query);
     // console.log(result);
